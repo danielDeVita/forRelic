2ca569d1246ab2af471e657ec3e19e2e94f227cf
danielDeVita <danielitodevita@gmail.com>
2024-05-10 15:00:16 -0300

    second commit

--- index.js
+++ index.js
@@ -2,11 +2,12 @@ const express = require('express')
 const app = express()
 const TelegramBot = require('node-telegram-bot-api');
 
+require('dotenv').config()
 app.use(express.json());
 
 app.listen(3000, () => console.log('up'))
 
-const bot = new TelegramBot(`6767664046:AAHkSfcrDSwYB80SxT-U_7FHV48mK6pycKU`, { polling: true });
+const bot = new TelegramBot(`${process.env.BOT_TOKEN}`, { polling: true });
 
 //TEST PING PONG
 app.get('/', async (req, res) => res.send('ok'))
@@ -17,7 +18,7 @@ app.post("/new-relic-alert", async (req, res) => {
 
     const alertMessage = req.body;
 
-    const chatId = '-4212280450';
+    const chatId = `${process.env.CHAT_ID}`;
 
 
     try {
